595dcc564379eded531d69bb90b5cea38a91a690
Murtuza Ranapur <mranapurwala56@gmail.com>
2018-04-07 17:56:52 +0530

    Adding apis to get and update string

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -3,6 +3,11 @@ package io.vertx.starter;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
@@ -10,8 +15,60 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> startFuture) {
-    HttpClient httpClient = vertx.createHttpClient();
+    server().setHandler(ar ->{
+      if(ar.succeeded()){
+        startFuture.complete();
+      }
+      else
+        startFuture.fail(ar.cause());
+    });
+  }
 
+  private Future<Void> server(){
+    Future<Void> serverFuture = Future.future();
+    HttpServer httpServer = vertx.createHttpServer();
+
+    Router router = Router.router(vertx);
+    router.get("/").handler(this::doNothing);
+    router.get("/getString").handler(this::getStringHandler);
+    router.post().handler(BodyHandler.create());
+    router.post("/update").handler(this::updateString);
+
+    httpServer
+      .requestHandler(router::accept)
+      .listen(8080, ar ->{
+        if (ar.succeeded()){
+          System.out.println("Started successfully on port 8080");
+          serverFuture.complete();
+        }
+        else
+        {
+          System.out.println("Failed due to "+ar.cause());
+          serverFuture.fail(ar.cause());
+        }
+      });
+    return serverFuture;
+  }
+
+  private void doNothing(RoutingContext routingContext) {
+    routingContext.response().setStatusCode(200);
+    routingContext.response().putHeader("Content-Type", "application/json");
+    routingContext.response().end(new JsonObject().encodePrettily());
+  }
+
+  private void updateString(RoutingContext routingContext) {
+    JsonObject getObject = routingContext.getBodyAsJson();
+    userString = getObject.getString("string");
+    routingContext.response().setStatusCode(201);
+    routingContext.response().putHeader("Content-Type", "application/json");
+    routingContext.response().end(new JsonObject().put("success", true).encode());
+  }
+
+  private void getStringHandler(RoutingContext routingContext) {
+    JsonObject jsonObject = new JsonObject().put("string", userString);
+    routingContext.response().setStatusCode(200);
+    routingContext.response().putHeader("Content-Type", "application/json");
+    routingContext.response().end(jsonObject.encodePrettily());
   }
 
 }
