e449e22be8a148822ba279312b9229488a0b4161
Murtuza Ranapur <mranapurwala56@gmail.com>
2018-04-07 17:16:31 +0530

    Adding new dependencies

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -1,14 +1,17 @@
 package io.vertx.starter;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpClient;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private String userString;
+
   @Override
-  public void start() {
-    vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x!"))
-        .listen(8080);
+  public void start(Future<Void> startFuture) {
+    HttpClient httpClient = vertx.createHttpClient();
+
   }
 
 }
